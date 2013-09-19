f455b801385626cf3814864edb1fe2a1c5a29ecb
Peter <peterdlugt@live.nl>
2013-09-19 23:39:22 +0200

    changed font and added pirate sprite

--- project/assets/lua/loader/styles.lua
+++ project/assets/lua/loader/styles.lua
@@ -1,12 +1,12 @@
 -- 4. Font.1 nieuwe font aanmaken
 font = MOAIFont.new()
-font:load("resources/arial.ttf")
+font:load("resources/TinyPixy.ttf")
  
 charcodes = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,!?"
 
 -- font 2. Preload Glyphs (lettertjes die gerenderd zijn als plaatje)
-font:preloadGlyphs(charcodes, 10)
+font:preloadGlyphs(charcodes, 15)
 
 style = MOAITextStyle.new()
 style:setFont(font)
-style:setSize(10)
+style:setSize(15)
