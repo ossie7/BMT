e166a5ced5a66d5d12db3b1b4e5c9683e0ede7cf
Oswin <oswin_msn@hotmail.com>
2013-09-25 13:41:42 +0200

    Shorten texture creation

--- project/assets/lua/loader/texturemgr.lua
+++ project/assets/lua/loader/texturemgr.lua
@@ -1,67 +1,23 @@
 function initTextures()
-  truck = MOAIImage.new()
-  truck:load("resources/Ship.png")
-  sprite = MOAIGfxQuad2D.new() -- Player
-  sprite:setTexture(truck)
-  sprite:setRect(-16,-16,16,16)
+  --name          path                         x1    y1   x2   y2
+  sprite     = cs("resources/ship.png",        -16,  -16, 16,  16) --Player
+  bsprite    = cs("resources/bullet_1.png",    -2,   -2,  2,   2)  --Bullet
+  e1sprite   = cs("resources/left_small.png",  -12,  -12, 12,  12) --Enemy Left
+  e2sprite   = cs("resources/right_small.png", -12,  -12, 12,  12) --Enemy Right
+  csprite    = cs("resources/target.png",      -8,   -8,  8,   8)  --Crosshair
+  gunsprite  = cs("resources/weapons.png",     -16,  -16, 16,  16) --Gun
+  lbsprite   = cs("resources/blue_bar.png",    0,    0,   150, -8) --Left Bar
+  rbsprite   = cs("resources/red_bar.png",     -150, -8,  0,   0)  --Right Bar
+  basesprite = cs("resources/base.png",        -160, -90, 160, 90) --Base Background
+  lfsprite   = cs("resources/blue_fist.png",   -22,  -11, 0,   11) --Left Fist
+  rfsprite   = cs("resources/red_fist.png",    0,    -11, 22,  11) --Right Fist
+end
 
-  texture = MOAIImage.new()
-  texture:load("resources/bullet_1.png")
-  bsprite = MOAIGfxQuad2D.new() -- Bullet
-  bsprite:setTexture(texture)
-  bsprite:setRect(-2,-2,2,2)
-  
-  e1texture = MOAIImage.new()
-  e1texture:load("resources/left_small.png")
-  e1sprite = MOAIGfxQuad2D.new() -- Enemy left
-  e1sprite:setTexture(e1texture)
-  e1sprite:setRect(-12,-12,12,12)
-  
-  e2texture = MOAIImage.new()
-  e2texture:load("resources/right_small.png")
-  e2sprite = MOAIGfxQuad2D.new() -- Enemy right
-  e2sprite:setTexture(e2texture)
-  e2sprite:setRect(-12,-12,12,12)
-
-  crosstexture = MOAIImage.new()
-  crosstexture:load("resources/target.png")
-  csprite = MOAIGfxQuad2D.new() -- Crosshair
-  csprite:setTexture(crosstexture)
-  csprite:setRect(-8,-8,8,8)
-
-  guntexture = MOAIImage.new()
-  guntexture:load("resources/Ship_weapons.png")
-  gunsprite = MOAIGfxQuad2D.new() -- Gun
-  gunsprite:setTexture(guntexture)
-  gunsprite:setRect(-16,-16,16,16)
-  
-  lbtexture = MOAIImage.new()
-  lbtexture:load("resources/blue_bar.png")
-  lbsprite = MOAIGfxQuad2D.new() -- Left bar
-  lbsprite:setTexture(lbtexture)
-  lbsprite:setRect(0,0,150,-8)
-  
-  rbtexture = MOAIImage.new()
-  rbtexture:load("resources/red_bar.png")
-  rbsprite = MOAIGfxQuad2D.new() -- Right bar
-  rbsprite:setTexture(rbtexture)
-  rbsprite:setRect(-150,-8,0,0)
-  
-  basetexture = MOAIImage.new()
-  basetexture:load("resources/base.png")
-  basesprite = MOAIGfxQuad2D.new() -- Base background
-  basesprite:setTexture(basetexture)
-  basesprite:setRect(-160,-90,160,90)
-  
-  lftexture = MOAIImage.new()
-  lftexture:load("resources/blue_fist.png")
-  lfsprite = MOAIGfxQuad2D.new() -- Left fist
-  lfsprite:setTexture(lftexture)
-  lfsprite:setRect(-22,-11,0,11)
-  
-  rftexture = MOAIImage.new()
-  rftexture:load("resources/red_fist.png")
-  rfsprite = MOAIGfxQuad2D.new() -- Right fist
-  rfsprite:setTexture(rftexture)
-  rfsprite:setRect(0,-11,22,11)
+function cs(path, x1, y1, x2, y2) -- Create sprite
+  local texture = MOAIImage.new()
+  texture:load(path)
+  local sprite = MOAIGfxQuad2D.new()
+  sprite:setTexture(texture)
+  sprite:setRect(x1, y1, x2, y2)
+  return sprite
 end
