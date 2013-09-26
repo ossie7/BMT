72dc8f9143667864379d16b9f577c3954806a9bc
DijkeMark <mark.dijkema@gmail.com>
2013-09-26 12:30:38 +0200

    Buying shipupgrades now costs resources
    Buying shipupgrades now costs resources and the Build button now only
    shows up if the players has enough resources and it has not bought it
    yet

--- project/assets/lua/helper/upgrade_helper.lua
+++ project/assets/lua/helper/upgrade_helper.lua
@@ -1,17 +1,40 @@
 function BuildShipUpgrade()
   local upgrade = shipUpgradesList[lastTappedShipUpgrade]
-  upgrade:Build(true)
+  
+  if EnoughResources(upgrade) then
+    local metal = upgrade:GetRequiredMetal()
+    local tech = upgrade:GetRequiredTech()
+    
+    userdata.metal = userdata.metal - metal
+    userdata.tech = userdata.tech - tech
+    
+    upgrade:Build(true)
+  end
   
   save_userdata()
   
   return upgrade
 end
 
+function EnoughResources(upgrade)
+  local metal = upgrade:GetRequiredMetal()
+  local tech = upgrade:GetRequiredTech()
+  
+  if userdata.metal >= metal and userdata.tech >= tech then
+    return true
+  else
+    return false
+  end
+end
+
 function SetBuildButtonVisibility(upgrade)
-  print("SetBuildButtonVisibility")
   if upgrade:IsBuild() then
     propBuildButton:setVisible(false)
   else
-    propBuildButton:setVisible(true)
+    if EnoughResources(upgrade) then
+      propBuildButton:setVisible(true)
+    else
+      propBuildButton:setVisible(false)
+    end
   end
 end
